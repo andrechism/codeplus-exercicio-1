ba27681f22de5408109bfc42c81204b129ee32fa
André Aragão <andrearagaoart@gmail.com>
2020-12-01 20:47:52 -0300

    function listDevs implementada

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const eplusDevList = ['Parça', 'Galego', 'Xará', 'Baqueta', 'Na', 'Lindo', 'Pablito', 'Bahia']
 
 function listDevs(list){
-
+  list.forEach(dev => console.log(dev));
 }
 
 listDevs(eplusDevList)
